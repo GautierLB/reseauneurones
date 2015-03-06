0fbb77268fcc873e9caabc5120d5d529f2f0d2d4
gautier.lb@gmail.com <gautier.lb@gmail.com@a02bf148-afa9-15bb-b936-83fe7ed59d05>
2015-03-06 14:38:34 +0000

    ** Gautier **
    création des classes neurones et reseau de neurones

---
+++ trunk/SurvieCancer/Neurone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvieCancer
+{
+    class Neurone
+    {
+        private int[] poids;
+
+        public Neurone()
+        {
+            this.poids = new int[4];
+        }
+    }
+}
