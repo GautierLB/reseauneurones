b379ccf3ce6da6a53ae6b93d5ad9dc438d372992
leojf.bouchet@gmail.com <leojf.bouchet@gmail.com@a02bf148-afa9-15bb-b936-83fe7ed59d05>
2015-03-06 14:35:55 +0000

    Ajout de la classe entrée FileReader et split pour l'entrée*

---
+++ SurvieCancer/entrée.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvieCancer
+{
+    class entrée
+    {
+        private int agePatient;
+
+        public int AgePatient
+        {
+            get { return agePatient; }
+            set { agePatient = value; }
+        }
+        private int anneeOperation;
+
+        public int AnneeOperation
+        {
+            get { return anneeOperation; }
+            set { anneeOperation = value; }
+        }
+        private int nbGanglions;
+
+        public int NbGanglions
+        {
+            get { return nbGanglions; }
+            set { nbGanglions = value; }
+        }
+    }
+}
