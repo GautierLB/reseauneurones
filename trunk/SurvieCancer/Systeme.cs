766c07092ee8d747bc97bbc37263a5a1bd5d0fde
gautier.lb@gmail.com <gautier.lb@gmail.com@a02bf148-afa9-15bb-b936-83fe7ed59d05>
2015-03-11 11:45:33 +0000

    ** Gautier **
    Séparation des entrées entre la collection d'apprentissage(70%) et celle de test(30%)

--- trunk/SurvieCancer/Systeme.cs
+++ trunk/SurvieCancer/Systeme.cs
@@ -9,6 +9,7 @@ namespace SurvieCancer
     class Systeme
     {
         Entree[] entrees;
+        Entree[] entreesVerif;
         ReseauNeurone reseau;
 
 
@@ -16,7 +17,18 @@ namespace SurvieCancer
 
         public Systeme(Entree[] _entrees)
         {
-            this.entrees = _entrees;
+            entrees = new Entree[(int)Math.Ceiling(_entrees.Length * 0.7)];
+            entreesVerif = new Entree[(int)Math.Ceiling(_entrees.Length * 0.3)];
+            for (int i = 0; i < (int)Math.Ceiling(_entrees.Length * 0.7); i++)
+            {
+                this.entrees[i] = _entrees[i];
+            }
+            int a =0;
+            for (int j = (int)Math.Ceiling(_entrees.Length * 0.7); j < _entrees.Length ; j++)
+            {
+                this.entreesVerif[a] = _entrees[j];
+                a++;
+            }
             this.reseau = new ReseauNeurone();
         }
 
